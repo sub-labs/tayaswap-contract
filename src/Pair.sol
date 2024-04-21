fcb9c9faa9d85ccd88bf13c3f7f8f9217f3a913e
Whanod <whanod@gmail.com>
2024-04-21 15:54:57 +0330

    add pair files

---
+++ src/Pair.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+import "forge-std"
+
+contract Pair {
+    uint private reserv0;
+    uint private reserve1;
+
+
+
+    function mint() public {
+       
+
+    }
+
+}
