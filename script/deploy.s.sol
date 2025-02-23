d7203dbf0b6d258f6f4951228a11a2ba998cc782
eabz <eabz@kindynos.mx>
2025-02-23 12:20:28 -0600

    Deploy script

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -4,11 +4,13 @@ import "forge-std/Script.sol";
 pragma solidity 0.8.25;
 
 import "../src/SwapFactory.sol";
+import "../src/SwapRouter.sol";
 
 contract DeployFactory is Script {
-    function run() external returns (SwapFactory factory) {
+    function run() external returns (SwapFactory factory, SwapRouter router) {
         vm.startBroadcast();
         factory = new SwapFactory(address(msg.sender));
+        router = new SwapRouter(address(factory), address(0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701));
         vm.stopBroadcast();
     }
 }
